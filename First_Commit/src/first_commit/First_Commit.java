0b58ece3a477c2b8432aa5489c5b9d3ed3ce41ed
LilianMinny <S569482@nwmissouri.edu>
2024-01-25 16:27:00 -0600

    First Commit

---
+++ First_Commit/src/first_commit/First_Commit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package first_commit;
+
+/**
+ *
+ * @author S569482
+ */
+public class First_Commit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello!! Northwest");
+    }
+    
+}
